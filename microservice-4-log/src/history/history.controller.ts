90630f25edc6cd173cbb51d1c460e7d09cacdf31
Timor Bayu Saktiawan <timor.x@gmail.com>
2025-02-27 09:25:25 +0700

    menambahkan guard di history pada microservice-4-log kecuali POST

--- microservice-4-log/src/history/history.controller.ts
+++ microservice-4-log/src/history/history.controller.ts
@@ -6,8 +6,10 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { HistoryService } from './history.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('history')
 export class HistoryController {
@@ -18,16 +20,19 @@ export class HistoryController {
     return this.historyService.create(history);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async findAll() {
     return this.historyService.get();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Put(':id')
   async update(@Body() history: Partial<History>, @Param('id') id: string) {
     return this.historyService.update(id, history);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return this.historyService.delete(id);
--- microservice-4-log/src/history/history.module.ts
+++ microservice-4-log/src/history/history.module.ts
@@ -3,12 +3,13 @@ import { HistoryService } from './history.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HistorySchema } from './history.schema';
 import { HistoryController } from './history.controller';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'History', schema: HistorySchema }]),
   ],
-  providers: [HistoryService],
+  providers: [HistoryService, JwtStrategy],
   controllers: [HistoryController],
 })
 export class HistoryModule {}
